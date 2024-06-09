f57e321103e83934a348cf8f3f459e3b504e9499
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2024-06-09 19:44:40 -0300

    Add dt functions

--- src/components/Form/DataTableDT.tsx
+++ src/components/Form/DataTableDT.tsx
@@ -1,101 +1,185 @@
 import React from 'react'
 import FootballSevice from '../../services';
-import { TeamTypes } from '../../types';
+import { DTTypes, TeamTypes } from '../../types';
 import Title from '../Title';
 
 const DataTableDT = () => {
 
-    const [teams, setTeams] = React.useState<TeamTypes[]>(
-        FootballSevice.getTeam()
-      );
-      const [editingTeamId, setEditingTeamId] = React.useState<number | null>(null);
+    const [dts, setDts] = React.useState<DTTypes[]>(
+        FootballSevice.getDts()
+    );
+
+    const [editingDtId, setEditingDtId] = React.useState<number | null>(null);
+    const [editedDTName, setEditedDTName] = React.useState<string>("");
+    const [editedDTLastname, setEditedDTLastname] = React.useState("");
+    const [editedDTTeam, setEditedDTTeam] = React.useState<DTTypes>();
+    const [editedDTFormation, setEditedDTFormation] = React.useState<string>("");
+
+      const handleEditCancel=()=>{
+        setEditingDtId(null);
+      }
+
+      const handleEditSave =(id:number)=>{
+        if(editedDTName.trim() !== ""){
+          const updateDT = FootballSevice.updateDt({
+            id,
+            name: editedDTName,
+            lastName: editedDTLastname,
+            formation: editedDTFormation
+          });
+
+          setDts((prevDT) => prevDT.map((dt)=>(dt.id == id? updateDT : dt)));
+          setEditingDtId(null);
+        }
+      }
+
+      const handleEditStart=(
+        id: number,
+        name: string, 
+        lastname: string, 
+        formation: string,
+        // team?:TeamTypes, 
+        )=>{
+          setEditingDtId(id);
+          setEditedDTName(name);
+          setEditedDTLastname(lastname);
+          setEditedDTFormation(formation);
+      }
+
+      const handleDeleteDT =(id:number)=>{
+        FootballSevice.deleteDt(id);
+        setDts((prevdt) => prevdt.filter((dt) => dt.id !== id));
+      }
 
     return (
         <div className="flex flex-col gap-6 w-[60%] mt-[5%]">
           <Title titulo="DTS" />
     
           <div className="grid grid-cols-4 gap-6">
-            {teams.length == 0 ? (
+            {dts.length == 0 ? (
               <div>
                 <h4 className="font-mont font-medium text-white text-center text-md">
                   There are not DTs
                 </h4>
               </div>
             ) : (
-              teams.map((team) => (
+              dts.map((dt) => (
                 <div
-                  key={team.id}
+                  key={dt.id}
                   className="bg-white bg-opacity-10 rounded-md backdrop-blur-md p-4"
                 >
-                  {editingTeamId === team.id ? (
+                  {editingDtId === dt.id ? (
                     <div className="flex flex-col gap-2">
                       <fieldset className="w-full flex flex-col">
                         <label className="font-mont font-thin text-gray-100">
-                          Team
+                          DT
                         </label>
                         <input
                           className="p-1 rounded-md font-mont font-medium bg-gray-300 outline-none focus:bg-white"
                           type="text"
-                        //   value={editedTeamName}
-                        //   onChange={(e) => setEditedTeamName(e.target.value)}
+                          value={editedDTName}
+                          onChange={(e) => setEditedDTName(e.target.value)}
                         />
                       </fieldset>
-                  
     
-                      <fieldset>
-                        <span className="font-mont font-thin text-gray-100">
-                          DT
-                        </span>
-                        <span className="font-mont font-medium text-white">
-                          {team.dt?.name}
-                        </span>
+                      <fieldset className="w-full flex flex-col">
+                        <label className="font-mont font-thin text-gray-100">
+                          Last name
+                        </label>
+                        <input
+                          className="p-1 rounded-md font-mont font-medium bg-gray-300 outline-none focus:bg-white"
+                          type="text"
+                          value={editedDTLastname}
+                          onChange={(e) => setEditedDTLastname(e.target.value)}
+                        />
+                      </fieldset>
+
+                      <fieldset className="flex flex-col">
+                        <span className='text-white'>Team</span>
+                        <select 
+                          className='p-1 rounded-md bg-gray-300 outline-none focus:bg-white'
+                          // onChange={(e)=>setNewDTPosition(e.target.value)}
+                        >
+                          <option selected>Manchester</option>
+                          <option>Boca</option>
+                          <option>Real Madrid</option>
+                          <option>Flamengo</option>
+                        </select>
+                      </fieldset>
+
+                      <fieldset className="flex flex-col">
+                        <legend className='font-mont font-thin text-gray-100'
+                        >Formation</legend>
+                          <input 
+                            type="text"
+                            className='p-1 rounded-md bg-gray-300 outline-none focus:bg-white'
+                            placeholder='Add formation'
+                            value={editedDTFormation}  
+                            onChange={(e)=>setEditedDTFormation(e.target.value)}
+                            // focus={true}
+                          />
                       </fieldset>
     
-                   
                       <fieldset className="w-full flex flex-row gap-2 mt-8">
                         <button
                           className="w-full font-mont font-medium text-white border-2 bg-red-400 border-red-400 rounded-md p-1 hover:bg-red-500 hover:border-red-500 hover:text-white"
-                        //   onClick={() => handleEditCancel()}
+                          onClick={() => handleEditCancel()}
                         >
                           Cancel
                         </button>
                         <button
                           className="w-full font-mont font-medium text-white border-2 bg-green-400 border-green-400 rounded-md p-1 hover:bg-green-500 hover:border-green-500 hover:text-white"
-                        //   onClick={() => handleEditSave(team.id)}
+                          onClick={() => handleEditSave(dt.id)}
                         >
                           Save
                         </button>
                       </fieldset>
                     </div>
-                  ) : (
+                  ) : ( 
                     <div className="flex flex-col gap-2">
-                      <div className="w-full flex flex-col">
-                        <span className="font-mont font-thin text-gray-100">
-                          Team
-                        </span>
+                      <fieldset className="w-full flex flex-col">
+                        <label className="font-mont font-thin text-gray-100">DT:</label>
                         <span className="font-mont font-medium text-white">
-                          {team.name}
-                        </span>
-                      </div>
-                      <div className="w-full flex flex-col">
-                        <span className="font-mont text-gray-400">Stadium</span>
-                        <span className="text-white">{team.stadium}</span>
-                      </div>
-                      <div className="w-full flex flex-col">
-                        <span className="font-mont text-gray-400">DT</span>
-                        <span>{team.dt?.name}</span>
-                      </div>
-                      
+                          {dt.name} 
+                          </span>
+                      </fieldset>
+                      <fieldset className="w-full flex flex-col">
+                        <label className="font-mont text-gray-400">Last name:</label>
+                          <span className="text-white">
+                            {dt.lastName}
+                          </span>
+                      </fieldset>
+                      <fieldset className="w-full flex flex-col">
+                        <label className="font-mont text-gray-400">Formation:</label>
+                          <span className="text-white">
+                            {dt.formation}
+                          </span>
+                      </fieldset>
+                      <fieldset className="w-full flex flex-col">
+                        <label className="font-mont text-gray-400">Team:</label>
+                          <span className="text-white">
+                            Team
+                          </span>
+                      </fieldset>
                       <div className="w-full flex flex-row gap-2 mt-8">
                         <button
                           className="w-full font-mont font-medium text-white border-2 bg-blue-400 border-blue-400 rounded-md p-1 hover:bg-blue-500 hover:border-blue-500 hover:text-white"
-                        //   onClick={() => handleEditStart(team.id, team.name)}
+                          onClick={() =>
+                            handleEditStart(
+                              dt.id,
+                              dt.name,
+                              dt.lastName,
+                              // dt.team,
+                              dt.formation
+                            )
+                          }
                         >
                           Edit
                         </button>
+    
                         <button
                           className="w-full font-mont font-medium text-white border-2 bg-red-400 border-red-400 rounded-md p-1 hover:bg-red-500 hover:border-red-500 hover:text-white"
-                        //   onClick={() => handleDeleteTeam(team.id)}
+                          onClick={() => handleDeleteDT(dt.id)}
                         >
                           Delete
                         </button>
@@ -105,7 +189,7 @@ const DataTableDT = () => {
                 </div>
               ))
             )}
-          </div>
+        </div>
         </div>
       );
     };
